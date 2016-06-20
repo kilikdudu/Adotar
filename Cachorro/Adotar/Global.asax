4e04a4f0eb79ae5825680c6e1924741f7e664315
ClubManagement <carlosedu.ufg@gmail.com>
2016-06-20 01:19:37 -0300

    init

---
+++ Cachorro/Adotar/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Adotar.MvcApplication" Language="C#" %>
---
+++ Cachorro/Adotar/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Adotar.Startup))]
+namespace Adotar
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
