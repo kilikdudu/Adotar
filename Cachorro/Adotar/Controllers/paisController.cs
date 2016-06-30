aa5237287e985a2995b09a8766fdc29fa2169bcd
ClubManagement <carlosedu.ufg@gmail.com>
2016-06-30 19:22:33 -0300

    30/06/2016
    BODY BUILDER PORRA.

---
+++ Cachorro/Adotar/Controllers/paisController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Adotar;
+
+namespace Adotar.Controllers
+{
+    public class paisController : Controller
+    {
+        private adotarEntities db = new adotarEntities();
+
+        // GET: pais
+        public ActionResult Index()
+        {
+            return View(db.pais.ToList());
+        }
+
+        // GET: pais/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            pais pais = db.pais.Find(id);
+            if (pais == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pais);
+        }
+
+        // GET: pais/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: pais/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,descricao")] pais pais)
+        {
+            if (ModelState.IsValid)
+            {
+                db.pais.Add(pais);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pais);
+        }
+
+        // GET: pais/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            pais pais = db.pais.Find(id);
+            if (pais == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pais);
+        }
+
+        // POST: pais/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,descricao")] pais pais)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pais).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pais);
+        }
+
+        // GET: pais/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            pais pais = db.pais.Find(id);
+            if (pais == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pais);
+        }
+
+        // POST: pais/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            pais pais = db.pais.Find(id);
+            db.pais.Remove(pais);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Cachorro/Adotar/Models/CadastroAnimal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Adotar.Models
+{
+    public class CadastroAnimal
+    {
+        public string Nome { get; set; }
+        public long Raca_id { get; set; }
+        public long Idade { get; set; }
+        public long Sexo_id { get; set; }
+        public string Cor { get; set; }
+        public string Detalhes { get; set; }
+        public HttpPostedFileBase Foto { get; set; }
+    }
+}
